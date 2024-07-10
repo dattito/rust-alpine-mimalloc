0b0f34f290412d24e31108b4934d20122e8dc380
dattito <32016658+dattito@users.noreply.github.com>
2024-07-10 11:23:24 +0200

    fix: correct build targets

--- set_target.sh
+++ set_target.sh
@@ -9,18 +9,22 @@ mkdir /.cargo
 rust_target="unknown"
 
 if [ "${TARGETARCH}" = "amd64" ]; then
-  rust_target="x86_64-unknown-linux-musl";
+  rust_target="x86_64-alpine-linux-musl";
 fi
 
 if [ "${TARGETARCH}" = "arm64" ];then 
-  rust_target="aarch64-unknown-linux-musl";
+  rust_target="aarch64-alpine-linux-musl";
 fi
 
 if ["${rust_target}" = "unknown"]; then
+  echo "Architecture not yet supported / tested. Good luck."
   exit 0
 fi
 
 cat <<EOF >> /.cargo/config.toml
 [build]
 target="${rust_target}"
+
+[target.${rust_target}]
+rustflags = ["-C", "target-feature=+crt-static"]
 EOF
